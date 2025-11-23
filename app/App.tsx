7908668b60a770adc0b88858f5183f30547cf142
Martin <74210211+martinbrix60@users.noreply.github.com>
2025-11-23 18:15:07 +0100

    Change main layout alignment to justify-end
    Updated main layout to justify content to the end.

--- app/App.tsx
+++ app/App.tsx
@@ -24,9 +24,8 @@ export default function App() {
   }, []);
 
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-start bg-slate-100 dark:bg-slate-950">
-
-      <div className="relative mx-auto w-full max-w-5xl pt-20">
+    <main className="flex min-h-screen flex-col items-center justify-end bg-slate-100 dark:bg-slate-950">
+      <div className="mx-auto w-full max-w-5xl">
 
         <Link href="/" className="absolute top-3 left-3 z-10">
           <Image src={Icon} alt="Logo" width={232} height={46} />
